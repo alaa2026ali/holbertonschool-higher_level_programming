bf08d28cea2644d0b388fec8f4eca45c81bdf461
alaa2026ali <ali_alaa18@icloud.com>
2026-06-02 07:56:01 +0000

    Add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+argv = sys.argv
+
+count = len(argv) - 1
+
+if __name__ == "__main__":
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
