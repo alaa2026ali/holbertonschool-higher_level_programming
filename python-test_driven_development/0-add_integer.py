e83b0eaf84147e75c67608fce4136b56620ae8d0
alaa2026ali <ali_alaa18@icloud.com>
2026-06-11 00:25:14 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module that contains the add_integer function."""
+import math
 
 
 def add_integer(a, b=98):
@@ -7,10 +8,11 @@ def add_integer(a, b=98):
 
     Floats are cast to integers before addition.
     """
-    if type(a) not in [int, float]:
+    # Reject non-numbers, infinity, or NaN values
+    if type(a) not in [int, float] or math.isinf(a) or math.isnan(a):
         raise TypeError("a must be an integer")
 
-    if type(b) not in [int, float]:
+    if type(b) not in [int, float] or math.isinf(b) or math.isnan(b):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
