24d69f02e00860a7119c29fea2ce6fcffc7d7699
alaa2026ali <ali_alaa18@icloud.com>
2026-06-10 23:57:16 +0300

    Change isinstance to type for type checking

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,10 +7,10 @@ def add_integer(a, b=98):
 
     Floats are cast to integers before addition.
     """
-    if not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
