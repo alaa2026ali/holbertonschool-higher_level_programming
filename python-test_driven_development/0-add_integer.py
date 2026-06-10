1e5cd1c849a7fc1032774a84a2842c95fb2bf559
alaa2026ali <ali_alaa18@icloud.com>
2026-06-11 00:57:33 +0300

    Enhance type checking in add_integer function
    Updated type checks to include inf and NaN values.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
 This module provides a function to add two numbers.
-It enforces type checking for integers and floats.
+It enforces type checking for integers, floats, inf, and NaN.
 """
 
+
 def add_integer(a, b=98):
-    """
-    Adds two integers. Floats are casted to integers before addition.
-    
-    Raises a TypeError if either argument is not an integer or float.
+    """Adds two integers. Floats are casted to integers before addition.
+
+    Raises a TypeError if either argument is not a valid integer or float.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-        
+
+      if a != a or a in (float("inf"), float("-inf")):
+        raise TypeError("a must be an integer")
+    if b != b or b in (float("inf"), float("-inf")):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
