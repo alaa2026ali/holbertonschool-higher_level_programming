667eb7f8b8296c404d4be9f0c8ffe502cc618714
alaa2026ali <ali_alaa18@icloud.com>
2026-06-11 00:58:55 +0300

    Refactor docstrings in add_integer function
    Updated docstrings for clarity and consistency.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""
-This module provides a function to add two numbers.
-It enforces type checking for integers, floats, inf, and NaN.
-"""
+"""Module that contains the add_integer function."""
 
 
 def add_integer(a, b=98):
-    """Adds two integers. Floats are casted to integers before addition.
+    """Add two integers or floats (floats are cast to int)."""
 
-    Raises a TypeError if either argument is not a valid integer or float.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
 
-      if a != a or a in (float("inf"), float("-inf")):
-        raise TypeError("a must be an integer")
-    if b != b or b in (float("inf"), float("-inf")):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
