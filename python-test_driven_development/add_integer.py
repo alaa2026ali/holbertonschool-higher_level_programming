0a5b86857c8d4a64e40b1563ac02d44820205e17
alaa2026ali <ali_alaa18@icloud.com>
2026-06-10 21:33:43 +0000

    rm file

--- python-test_driven_development/add_integer.py
+++ python-test_driven_development/add_integer.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Module that contains the add_integer function."""
+"""
+Module containing add_integer function.
+"""
 
 
 def add_integer(a, b=98):
-    """Add two integers and return the result.
+    """
+    Add two integers.
 
     Floats are cast to integers before addition.
     """
-    # Check type, NaN (a != a), and Infinity without imports
-    if type(a) not in [int, float] or a != a or abs(a) == float('inf'):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if type(b) not in [int, float] or b != b or abs(b) == float('inf'):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
