c56b26a38d375943254684558264089732c8e12b
alaa2026ali <ali_alaa18@icloud.com>
2026-06-22 10:32:32 +0300

    Improve __str__ method formatting in Square class
    Refactor string representation for better readability.

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -18,4 +18,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return string representation of square."""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return "[Square] {}/{}".format(
+            self._Rectangle__width,
+            self._Rectangle__height
+        )
