2a08df780e94a2f95b55166dc08a0abb88facc78
alaa2026ali <ali_alaa18@icloud.com>
2026-06-24 10:50:01 +0300

    Refactor Circle class to use imported pi

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python3
 """Shapes, interfaces, and duck typing."""
 from abc import ABC, abstractmethod
-import math
+from math import pi
 
 
 class Shape(ABC):
-    """Abstract shape blueprint."""
-
     @abstractmethod
     def area(self):
-        """Return the area of the shape."""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Return the perimeter of the shape."""
         pass
 
 
 class Circle(Shape):
-    """Circle shape."""
-
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return pi * (self.radius ** 2)
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle shape."""
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -46,6 +38,5 @@ class Rectangle(Shape):
 
 
 def shape_info(shape):
-    """Print area and perimeter using duck typing."""
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
