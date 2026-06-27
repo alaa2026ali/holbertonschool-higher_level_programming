8ed0bc5097f954702076443a973965f9b9fa0d66
alaa2026ali <ali_alaa18@icloud.com>
2026-06-27 03:50:58 +0300

    Refactor Circle class and remove Shape inheritance
    Refactor Circle class to remove abstract base class and add circumference method.

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,37 +1,19 @@
-#!/usr/bin/env python3
-from abc import ABC, abstractmethod
-import math
+#!/usr/bin/python3
+"""Task 01 - Duck Typing"""
 
-class Shape(ABC):
-    @abstractmethod
-    def area(self):
-        pass
+from math import pi
 
-    @abstractmethod
-    def perimeter(self):
-        pass
 
-class Circle(Shape):
-    def __init__(self, radius):
+class Circle:
+    """Circle class"""
+
+    def __init__(self, radius=0):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        """Return the area of the circle"""
+        return pi * (abs(self.radius) ** 2)
 
-    def perimeter(self):
-        return 2 * math.pi * self.radius
-
-class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-
-def shape_info(shape):
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+    def circumference(self):
+        """Return the circumference of the circle"""
+        return 2 * pi * abs(self.radius)
