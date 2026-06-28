7e5344f3ca36af9165ff76e86aab58dd1ed9ef10
alaa2026ali <ali_alaa18@icloud.com>
2026-06-28 06:59:41 +0000

    Add 0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a function to read a UTF-8 text file."""
+
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its contents to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
