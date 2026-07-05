55aa3f384816db1d430e9c3cde9bc98451c31801
alaa2026ali <ali_alaa18@icloud.com>
2026-07-05 08:33:20 +0000

    Add task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        data = response.json()
+
+        for post in data:
+            print(post["title"])
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        posts = []
+
+        for post in data:
+            posts.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(posts)
