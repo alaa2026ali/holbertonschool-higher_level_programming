499e94dace9f5789463dd269ad1941fff8a930ee
alaa2026ali <ali_alaa18@icloud.com>
2026-07-24 00:55:48 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""Safely filter states by name"""
 
 import MySQLdb
 from sys import argv
 
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-                (argv[4], ))
-    query_rows = cur.fetchall()
-    for row in query_rows:
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+        (argv[4],)
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+
+    cursor.close()
+    db.close()
