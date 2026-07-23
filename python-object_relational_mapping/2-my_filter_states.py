2bc3c346a1bf8b790d63b3fbc82809284a72e238
alaa2026ali <ali_alaa18@icloud.com>
2026-07-23 20:33:21 +0000

    Fix task 2 output

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """Displays all values in the states table where name matches the argument."""
 
@@ -29,3 +30,34 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
+=======
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states "
+        WHERE name = '{}'
+        ORDER BY states.id ASC".format(sys.argv[4])
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
+
