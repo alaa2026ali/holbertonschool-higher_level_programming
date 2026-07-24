b92b8b973d26a98c783ce2aeb9c58655d4fd174c
alaa2026ali <ali_alaa18@icloud.com>
2026-07-24 03:41:39 +0300

    Refactor city fetching script to include ordering
    Updated the script to list all cities by state and added ordering by city ID.

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""List all states"""
+"""Lists all cities by state"""
+
 from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    results = session.query(City, State).\
-        filter(City.state_id == State.id).all()
+
+    results = session.query(City, State).filter(
+        City.state_id == State.id
+    ).order_by(City.id).all()
+
     for city, state in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.commit()
+
     session.close()
